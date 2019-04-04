e8812de8836ce5bd92f91102029797a99e735d16
JC Smiley <jcsmileyjr@gmail.com>
2019-04-04 12:59:20 -0500

    Scaffold the component based on wire-frame

--- cars/App.js
+++ cars/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Constants } from 'expo';
 import Admin_Login from './components/admin_login.js';
+import Admin_Homescreen from './components/admin_homescreen.js';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Admin_Login />
+        <Admin_Homescreen/>
       </View>
     );
   }
@@ -19,5 +20,5 @@ const styles = StyleSheet.create({
     paddingTop: Constants.statusBarHeight,
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
