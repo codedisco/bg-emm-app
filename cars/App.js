5466f6cb55860dc891c981583b82a6f07a5d416d
JC Smiley <jcsmileyjr@gmail.com>
2019-04-05 11:40:44 -0500

    Initial set up
    - Flatlist component working with a data source

--- cars/App.js
+++ cars/App.js
@@ -3,12 +3,13 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Constants } from 'expo';
 import Admin_Login from './components/admin_login.js';
 import Admin_Homescreen from './components/admin_homescreen.js';
+import Admin_One_Vehicle from './components/admin_one_vehicle.js';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Admin_Homescreen/>
+        <Admin_One_Vehicle />
       </View>
     );
   }
