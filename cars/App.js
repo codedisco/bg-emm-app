7662342e2ce9203e90c1ed7355838e781192aac0
JC Smiley <jcsmileyjr@gmail.com>
2019-04-10 20:40:10 -0500

    WIP

--- cars/App.js
+++ cars/App.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Constants } from 'expo';
+
 import Admin_Login from './components/admin_login.js';
 import Admin_Homescreen from './components/admin_homescreen.js';
 import Admin_One_Vehicle from './components/admin_one_vehicle.js';
 import Admin_Two_Vehicle from './components/admin_two_vehicle.js';
 
 export default class App extends React.Component {
+  constructor(props){
+    super();
+    this.state = {
+      pwd:"1111",//password for the admin_Login component's input
+      userEnterPassword:"",
+      }
+  }
+
+  //check if the player enter password is correct after four digits. This method is passed as a prop into the Admin_login component and used in its input button
+  playerLogin = (data) => {
+    //add each number entered by the user into the userEnterPassword state
+    let playerPwdLength = data.length;
+    
+    //if four numbers is entered, check if the password is correct with passwordChecker()
+    if(playerPwdLength == 4){
+        this.passwordChecker(data);
+    }
+      
+    //If incorrect, clear the input and start over
+      
+    //if correct, move to the Admin_homescreen component
+//console.log("It works " + playerPwdLength);      
+  }
+  
+  passwordChecker = (pwd) => {
+    if(pwd == this.state.pwd){
+console.log("Password Passes");
+    }else {
+console.log("Password Fail");
+    }
+  }
+  
+  
   render() {
     return (
       <View style={styles.container}>
-        <Admin_Two_Vehicle />
+        <Admin_Login login={this.playerLogin} />
       </View>
     );
   }
--- cars/components/admin_login.js
+++ cars/components/admin_login.js
@@ -8,27 +8,45 @@ import {
 
 import Admin_Nav from './admin_nav.js';
 
-export default function Admin_Login(props) {
-  return(
-    <View style={styles.container}>
-      <Admin_Nav navTitle = "Admin Setting" />
-      <View style={styles.loginInputBox}>
-        <View>
-            <Text style={styles.textColor}>
-                ENTER PASSWORD 
-            </Text>
+export default class Admin_Login extends React.Component {
+ constructor(props) {
+   super(props);
+   this.state = {
+     playerInput: ''
+   };
+ }
+    
+ checkPassword = (text) =>{
+    this.setState(previousState => (
+        { playerInput: text}
+      ))
+     
+     this.props.login(text);
+ }
+  render(){
+      return(
+        <View style={styles.container}>
+          <Admin_Nav navTitle = "Admin Setting" />
+          <View style={styles.loginInputBox}>
+            <View>
+                <Text style={styles.textColor}>
+                    ENTER PASSWORD 
+                </Text>
+            </View>
+            <View style={styles.inputBoxContainer}>
+                <TextInput
+                    value={this.state.playerInput}
+                    maxLength={4}
+                    autofocus = {true}
+                    secureTextEntry={true}      
+                    style={styles.inputStyle}
+                    onChangeText={(text) => {this.checkPassword(text)}}
+                /> 
+            </View>    
+          </View>
         </View>
-        <View style={styles.inputBoxContainer}>
-            <TextInput
-                autofocus = {true}
-                secureTextEntry={true}      
-                style={styles.inputStyle}
-              //onChangeText={(text) => {this.updateFullName(text)}}
-            /> 
-        </View>    
-      </View>
-    </View>
-  );
+      );      
+  }
 }
 
 const styles = StyleSheet.create({
