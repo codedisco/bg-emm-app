d9bd77aee8aa2ad47a2ef48f569f968bf6c041ed
Ted Patterson <tedjpatterson@gmail.com>
2019-04-13 15:23:38 -0500

    UI Setup

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+import { Col, Row, Grid } from 'react-native-easy-grid';
 
-export default class App extends React.Component {
+export default class AnatomyExample extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+            </Button>
+          </Left>
+          <Body>
+          </Body>
+          <Right>
+            <Title>Edge Motor Museum</Title>
+          </Right>
+        </Header>
+          <Grid>
+            <Col style={{ backgroundColor: '#635DB7', height: 600 }}>
+              <Content>
+                <Text>Option one</Text>
+              </Content>
+            </Col>
+            <Col style={{ backgroundColor: '#00CE9F', color: '#fff', height: 600 }}>
+              <Content>
+                <Text>Option Two</Text>
+              </Content>
+            </Col>
+          </Grid>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
