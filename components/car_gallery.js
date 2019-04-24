9abdc145e570def2d2d4b228361296ea684ad89c
JC Smiley <jcsmileyjr@gmail.com>
2019-04-24 18:45:31 -0500

    Adding more scaffolding

--- components/car_gallery.js
+++ components/car_gallery.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import Carousel from 'react-native-snap-carousel';
-import { Text, View, ImageBackground, Image } from 'react-native';
+import {ImageBackground, Image, TouchableOpacity } from 'react-native';
+import { Container, Content, Header, Left, Body, Right, Button, Icon, Title, Text, View, Col, Row, Grid } from 'native-base';
 
-export default class App extends React.Component {
+export default class Gallery extends React.Component {
     
   _renderItem ({item, index}) {
     return (
       <View >
           <Image
-            style={{width: 120, height: 70, resizeMode: 'contain', marginRight: 20}}
+            style={{width: 300, height: 300, resizeMode: 'contain', marginRight: 20}}
             source={item.photo}
           />
       </View>
@@ -18,12 +19,37 @@ export default class App extends React.Component {
 
   render () {
     return (
-      <Carousel
-        ref={(c) => { this._carousel = c; }}
-        data={this.props.cars}
-        renderItem={this._renderItem}
-        sliderWidth={150}
-        itemWidth={100}
-      />
+        <Container>
+            <Header noShadow style={{height: 80, backgroundColor: '#4F5961', paddingLeft: 0}}>
+                <Left>
+                  <TouchableOpacity onPress = {() =>{props.goBack()}} style={{backgroundColor: '#E5C035', margin: 0}}>
+                    <Image
+                      style={{width: 40, height: 40, margin: 20, resizeMode: 'contain',}}
+                      source={require('../assets/arrow-left.png')}
+                    />
+                  </TouchableOpacity>
+                </Left>
+                <Right>
+                  <Image
+                    style={{width: 120, height: 70, resizeMode: 'contain', marginRight: 20}}
+                    source={require('../assets/emm-logo-large.png')}
+                  />
+                </Right>
+            </Header>        
+            <Content>
+                <Grid>
+                    <Col>
+                      <Carousel
+                        ref={(c) => { this._carousel = c; }}
+                        data={this.props.cars}
+                        renderItem={this._renderItem}
+                        sliderWidth={600}
+                        itemWidth={400}
+                      />        
+                    </Col>
+                </Grid>
+            </Content>
+        </Container>
+
  ); 
 }}
