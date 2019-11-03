cfed27277d42428400370247afd00d6fe0148b1f
JC Smiley <jcsmileyjr@gmail.com>
2019-11-03 17:30:04 -0600

    Hide details of selectedCar2 marked as undefined

--- components/admin_two_vehicle.js
+++ components/admin_two_vehicle.js
@@ -165,7 +165,7 @@ export default function Admin_Two_Vehicle(props) {
                                     value={props.cars[18].id} />
                             </Picker>
                         </Button>
-                        {props.selectedCar1 !== undefined &&
+                        {props.selectedCar2 !== undefined &&
                             <Text style={{textAlign: 'center', fontSize: 20, color: '#ffffff', marginTop: 20, fontFamily: 'Inter-SemiBold'}}>
                                 {props.selectedCar2.year} {props.selectedCar2.make} {props.selectedCar2.model}
                             </Text>
